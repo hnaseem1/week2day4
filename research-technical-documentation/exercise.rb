f4b696e1ed602c6155433a41c8a8efd1af0220c7
Hasan Naseem <hn92@live.com>
2018-03-22 18:15:31 -0400

    exercise3

---
+++ research-technical-documentation/exercise.rb
@@ -0,0 +1,57 @@
+# string
+
+# length
+
+#returns the length of a character
+#0 arguments to give
+# "Hasan".length
+
+# strip
+#removes whitespace
+#0 arguments required
+
+
+# split
+#splits a string into an array based on the whitespace
+
+# start_with?
+#returns a boolean value if the argument is a string that matches with the string method is called on
+#Arguments required
+
+# Array
+
+# first
+# Returns the first element, or the first n elements, of the array. If the array is empty, the first form returns nil, and the second form returns an empty array.
+
+
+# delete_at
+# Deletes the element at the specified index, returning that element, or nil if the index is out of range. See also Array#slice!.
+
+# delete
+# Deletes items from self that are equal to obj. If any items are found, returns obj. If the item is not found, returns nil. If the optional code block is given, returns the result of block if the item is not found. (To remove nil elements and get an informative return value, use compact!)
+
+# pop
+# Removes the last element from self and returns it, or nil if the array is empty.
+# If a number n is given, returns an array of the last n elements (or less) just like array.slice!(-n, n) does.
+
+# Hash
+
+# to_a
+# converts hash to an array
+
+# has_key?
+# Returns true if the given key is present in hsh.
+
+# has_value?
+# Returns true if the given value is present in hsh.
+
+# Time
+# now
+# shows the current time
+
+# File
+# exist?
+# Return true if the named file exists.
+
+# extname
+# Returns the extension (the portion of file name in path starting from the last period).
